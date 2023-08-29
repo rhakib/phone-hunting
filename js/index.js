f516f2d4d47db873f05984b9ead4671f1a9eb34e
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2023-08-30 02:07:56 +0600

    added function to find phone

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`); // loadphone function k call korar somoi j arguments ta pathano hoice setar parameter dynamically bosano holo
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -7,10 +7,27 @@ const loadPhone = async () => {
 }
 
 const displayPhones = phones => {
-    const phoneContainer = document.getElementById('phone-container')
+    const phoneContainer = document.getElementById('phone-container');
+    //clear phone container card
+    phoneContainer.innerHTML = '';
+
+    // display show all btn if phones above 20 
+
+    const showAllContainer = document.getElementById('show-all-container');
+    if(phones.length > 12) {
+        showAllContainer.classList.remove('hidden');
+    } else {
+        showAllContainer.classList.add('hidden')
+    }
+
+
+    // display first 12 phones
+
+    phones = phones.slice(0,12)
+
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
-        phoneDiv.classList = 'card w-96 bg-base-100 shadow-xl'
+        phoneDiv.classList = 'card bg-base-100 p-4 shadow-xl'
         phoneDiv.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
         <div class="card-body text-center items-center">
@@ -26,5 +43,10 @@ const displayPhones = phones => {
 
 
 }
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchFieldValue = searchField.value;
+    console.log(searchFieldValue);
+    loadPhone(searchFieldValue); // upoer loadphone fuction ta call kora holo and arguments pathano holo
+}
 
-loadPhone();
