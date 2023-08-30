56047b6b7af9242c3da6db38b4ffe1fa1f6ce67b
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2023-08-30 13:10:35 +0600

    show all buttom added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`); // loadphone function k call korar somoi j arguments ta pathano hoice setar parameter dynamically bosano holo
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
-    displayPhones(phones)
+    displayPhones(phones,isShowAll)
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
     const phoneContainer = document.getElementById('phone-container');
     //clear phone container card
     phoneContainer.innerHTML = '';
@@ -14,16 +14,18 @@ const displayPhones = phones => {
     // display show all btn if phones above 20 
 
     const showAllContainer = document.getElementById('show-all-container');
-    if(phones.length > 12) {
+    if(phones.length > 12 && !isShowAll) {
         showAllContainer.classList.remove('hidden');
     } else {
         showAllContainer.classList.add('hidden')
     }
 
+     
+    // display first 12 phones if not Show All
 
-    // display first 12 phones
-
-    phones = phones.slice(0,12)
+    if(!isShowAll) {
+        phones = phones.slice(0,12)
+    }
 
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
@@ -40,13 +42,29 @@ const displayPhones = phones => {
         `
         phoneContainer.appendChild(phoneDiv)
     });
+    toggleLoadSpinner(false)
 
 
 }
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
+    toggleLoadSpinner(true)
     const searchField = document.getElementById('search-field');
     const searchFieldValue = searchField.value;
     console.log(searchFieldValue);
-    loadPhone(searchFieldValue); // upoer loadphone fuction ta call kora holo and arguments pathano holo
+    loadPhone(searchFieldValue, isShowAll); // upoer loadphone fuction ta call kora holo and arguments pathano holo
 }
 
+const toggleLoadSpinner = (isLoading) => {
+    const loadSpinner = document.getElementById('loading-spinner');
+    if(isLoading) {
+        loadSpinner.classList.remove('hidden')
+    }
+    else {
+        loadSpinner.classList.add('hidden')
+    }
+}
+
+// handle Show All 
+const showAllPhones = () => {
+    handleSearch(true)
+}
